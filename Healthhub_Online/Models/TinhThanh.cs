26e3d0dc874e1a62624102d934c3b34b0683b959
Bien Cuong <dogongocky@gmail.com>
2024-02-21 01:37:21 +0700

    models

---
+++ Healthhub_Online/Models/TinhThanh.cs
@@ -0,0 +1,31 @@
+namespace Healthhub_Online.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("TinhThanh")]
+    public partial class TinhThanh
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TinhThanh()
+        {
+            NguoiDungs = new HashSet<NguoiDung>();
+            TrungTamGanNhats = new HashSet<TrungTamGanNhat>();
+        }
+
+        [Key]
+        public int IDTinh { get; set; }
+
+        [StringLength(30)]
+        public string TenTinh { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<NguoiDung> NguoiDungs { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TrungTamGanNhat> TrungTamGanNhats { get; set; }
+    }
+}
