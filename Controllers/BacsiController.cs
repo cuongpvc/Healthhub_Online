8f83d404a387c51d75eb988c1dfe46fb05e36740
hhhiyu <q.huytran1607@gmail.com>
2024-02-22 22:20:25 +0700

    NguoidungController, BacsiController, NguoidungView, BacsiView update

--- Controllers/BacsiController.cs
+++ Controllers/BacsiController.cs
@@ -1,18 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
+using Healthhub_Online.Models;
+using Healthhub_Online.Models;
 
-namespace Healthhub_Online.Controllers
+namespace WebAppYte.Controllers
 {
     public class BacsiController : Controller
     {
+        private WebAppYteEntities db = new WebAppYteEntities();
+
+        // GET: Bacsi
         public ActionResult Index()
         {
-            return View();
+            var quanTris = db.QuanTris.Include(q => q.Khoa).Where(x => x.VaiTro == 2);
+            return View(quanTris.ToList());
+        }
+
+        // GET: Bacsi/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTris.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanTri);
+        }
+        // GET: Bacsi/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTris.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDKhoa = new SelectList(db.Khoas, "IDKhoa", "TenKhoa", quanTri.IDKhoa);
+            return View(quanTri);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDQuanTri,TaiKhoan,MatKhau,VaiTro,ThongTinBacSi,TrinhDo,IDKhoa,HoTen,AnhBia")] QuanTri quanTri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(quanTri).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDKhoa = new SelectList(db.Khoas, "IDKhoa", "TenKhoa", quanTri.IDKhoa);
+            return View(quanTri);
+        }
+
+        // GET: Admin/QuanTris/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTris.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanTri);
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
- 
--- Controllers/NguoidungController.cs
+++ Controllers/NguoidungController.cs
@@ -1,16 +1,83 @@
-﻿using System;
+﻿using Healthhub_Online.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
-namespace Healthhub_Online.Controllers
+namespace WebAppYte.Controllers
 {
     public class NguoidungController : Controller
     {
+        private WebAppYteEntities db = new WebAppYteEntities();
+
+        // GET: Nguoidung
         public ActionResult Index()
         {
-            return View();
+            var nguoiDungs = db.NguoiDungs.Include(n => n.GioiTinh).Include(n => n.TinhThanh);
+
+            return View(nguoiDungs.ToList());
+        }
+
+        // GET: Nguoidung/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguoiDung nguoiDung = db.NguoiDungs.Find(id);
+            if (nguoiDung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguoiDung);
+        }
+
+        // GET: Nguoidung/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguoiDung nguoiDung = db.NguoiDungs.Find(id);
+            if (nguoiDung == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDGioiTinh = new SelectList(db.GioiTinhs, "IDGioiTinh", "GioiTinh1", nguoiDung.IDGioiTinh);
+            ViewBag.IDTinh = new SelectList(db.TinhThanhs, "IDTinh", "TenTinh", nguoiDung.IDTinh);
+            return View(nguoiDung);
+        }
+
+        // POST: Nguoidung/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDNguoiDung,HoTen,Email,DienThoai,TaiKhoan,MatKhau,IDGioiTinh,DiaChiCuThe,SoCMND,IDTinh,NhomMau,ThongTinKhac")] NguoiDung nguoiDung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoiDung).State = EntityState.Modified;
+                db.SaveChanges();
+                ViewBag.capnhat = " Cập nhật thành công ";
+            }
+            ViewBag.IDGioiTinh = new SelectList(db.GioiTinhs, "IDGioiTinh", "GioiTinh1", nguoiDung.IDGioiTinh);
+            ViewBag.IDTinh = new SelectList(db.TinhThanhs, "IDTinh", "TenTinh", nguoiDung.IDTinh);
+            return View(nguoiDung);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
