5a214dea4ab33f474c220c6e1303b59def04f122
luongnvhe160166 <luongnvhe160166@fpt.edu.vn>
2024-03-11 19:43:51 +0700

    Change

--- Healthhub_Online/Controllers/HoidapController.cs
+++ Healthhub_Online/Controllers/HoidapController.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Healthhub_Online.Controllers
-{
-    public class HoidapController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-      
-    }
-}
+﻿
